0cf3d5e800c79f9b479ceec65b8157ffd60b6d03
Ernestoll484 <42830943+Ernestoll484@users.noreply.github.com>
2021-05-25 08:03:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,83 @@
-var spelers = [
-{image: "img/GK.png" , name: "Tim Krul" , id:1},
-{image: "img/MDL.png" , name: "Matthijs de Licht", id:2},
-{image: "img/OWL.png" , name:  "Owen Wijndal", id:3},
-{image: "img/DK.png" , name: "Davy Klaassen", id:4},
-{image: "img/GW.png" , name: "Georginio Wijndalum", id:5},
-{image: "img/MP.png" , name: "Memphis Depay", id:6},
-{image: "img/SB.png" , name: "Steven Berghuis", id:7},
-{image: "img/DB.png" , name: "Daley Blind", id:8},
-{image: "img/LdJ.png" , name: "Luuk de Jong", id:9},
-{image: "img/FdJ.png" , name: "Frenkie de Jong", id:10},
-{image: "img/DD.png" , name: "Denzel Dumfries", id:11},
-{image: "img/MB.png" , name: "Marco Bizot", id:12},
+var players = [
+{image: "img/GK.png" , name: "Tim Krul"  },
+{image: "img/MDL.png" , name: "Matthijs de Licht" },
+{image: "img/OWL.png" , name:  "Owen Wijndal" },
+{image: "img/DK.png" , name: "Davy Klaassen" },
+{image: "img/GW.png" , name: "Georginio Wijndalum" },
+{image: "img/MP.png" , name: "Memphis Depay" },
+{image: "img/SB.png" , name: "Steven Berghuis" },
+{image: "img/DB.png" , name: "Daley Blind" },
+{image: "img/LdJ.png" , name: "Luuk de Jong" },
+{image: "img/FdJ.png" , name: "Frenkie de Jong" },
+{image: "img/DD.png" , name: "Denzel Dumfries" }
 ];
-//maak een copie van alle fotos (slice)
-var idSpeler = [];
-var images = [];
-var names = [];
-var imageRow = [];
-var nameRow = [];
-var indexArrayImg = [];
-var indexArrayName = [];
 
-for (var i = 0; i < spelers.length; i++) {
-	images.push(spelers[i].image);
-	names.push(spelers[i].name);
-}    
-// zolang images nog een element bevat: vul een element met een image, verwijder daarna ook de image uit images
-function displayImages() {
-		// var tellerArray = 0
-		var teller = 0
-		var imgChosen = false
-	while  (images.length > 0 ){
-		let index = Math.floor(Math.random() * images.length);
-		var img = document.createElement("img");
-		let randomImage = images[index];
-		indexArrayImg.push(index)
-		img.setAttribute("src", randomImage);
-		document.getElementById("image-container").appendChild(img);
-		teller++;
-		img.id = "img" + teller;
-		img.className = "afb";
-		img.setAttribute("data-spelerNaam", names[index])
-	    imageRow.push(randomImage);
-		// tellerArray.push(teller);
-		// console.log(tellerArray)
-		img.addEventListener("click", selectImg(teller, imgChosen));
-		images.splice(index, 1);
-	};
+var selectedimage;
+var selectedName;
+var checkimage;
+var checkName;
+var countimage = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var countname = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const player = document.querySelectorAll(".players");
+const namePlayer = document.querySelectorAll(".namePlayer");
+
+
+//geeft border aan de spelers
+function selectimage(id){
+	if (selectedimage == undefined) {
+		//geeft witte border aan de speler
+		document.getElementById(id).classList.add("borders");
+		selectedimage = id;
+		checkimage = document.getElementById(id).src;
+	}else{
+		//removes border when another player or name is selected
+		document.getElementById(selectedimage).classList.remove("borders");
+		document.getElementById(id).classList.add("borders");
+		selectedimage = id;
+		checkimage = document.getElementById(id).src;
+	}
+	checkimage = checkimage.substring(checkimage.indexOf("img"));
+	console.log(checkimage);
+	checkMatch();
 }
-displayImages();
-displayName()
+
+player.forEach(player => {
+	//onclick voor despeler
+	player.onclick = function(){ selectimage(player.id); }
+});
+
+//Geeft border aan de namen
+function selectName(id){
+	if (selectedName == undefined) {
+		document.getElementById(id).classList.add("borderName");
+		selectedName = id;
+	}else{
+		document.getElementById(selectedName).classList.remove("borderName");
+		document.getElementById(id).classList.add("borderName");
+		selectedName = id;
+	}
+	checkName = document.getElementById(id).innerHTML;
+	console.log(checkName);
+	checkMatch();
+}
+
+namePlayer.forEach(namePlayer => {
+	//onclick voor de spelersnamen
+	namePlayer.onclick = function(){ selectName(namePlayer.id); }
+});
+
+//Random images
+for (var i=0; i<players.length; i++){
+	var random = countimage[Math.floor(Math.random() * countimage.length)];
+	var countCheck = countimage.indexOf(random);
+	countimage.splice(countCheck, 1);
+
+	document.getElementById("img-"+i).src= players[random].img;
+}
+//Random names 
+for (var i=0; i<players.length; i++){
+	var random = countname[Math.floor(Math.random() * countname.length)];
+	var countCheck = countname.indexOf(random);
+	countname.splice(countCheck, 1);
+	document.getElementById("btn-"+i).innerHTML= players[random].name;
+}
